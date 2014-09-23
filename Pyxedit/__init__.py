335d122d08ccea92ce47c957b9fd2513250ca760
Mike Clarke <mikedclarke109@gmail.com>
2014-09-23 23:52:41 +0100

    Changed project from tkinter to pyqt4

--- Pyxedit/__init__.py
+++ Pyxedit/__init__.py
@@ -1,50 +1,22 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 Author: Mike Clarke 13/09/14
 '''
 
-import Tkinter as tk
-import ScrolledText
-import tkFileDialog
-import tkMessageBox
+import sys
+from PyQt4 import QtGui
 
-main = tk.Tk(className=" Pyxedit")
-text_area = ScrolledText.ScrolledText(main, width=100, height=80)
+def main():
+    app = QtGui.QApplication(sys.argv)
 
-def open_cmd():
-    file = tkFileDialog.askopenfile(parent=main, mode='rb', title='Select a file')
-    if file != None:
-        contents = file.read()
-        text_area.insert('1.0', contents)
-        file.close()
+    w = QtGui.QWidget()
+    w.resize(800, 600)
+    w.move(300, 300)
+    w.setWindowTitle('Pyxedit')
+    w.show()
 
-def save_cmd(self):
-    file = tkFileDialog.asksaveasfile(mode='w')
-    data = self.text_area.get('1.0')
-    file.write(data)
-    file.close()
+    sys.exit(app.exec_())
 
-def exit():
-    if tkMessageBox.askokcancel('Exit', 'Are you sure?'):
-        main.destroy()
-
-def dummy():
-    pass
-
-menu = tk.Menu(main)
-main.config(menu=menu)
-filemenu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=dummy)
-filemenu.add_command(label="Open...", command=open_cmd)
-filemenu.add_command(label="Save", command=save_cmd)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=exit())
-helpmenu = tk.Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=dummy)
-# end of menu creation
-
-text_area.pack()
-main.mainloop()
-
-
+if __name__ == '__main__':
+    main()
