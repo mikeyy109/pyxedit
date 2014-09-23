a00abcb6c3cae17da62aa483d29f4e502b4de8ab
Mike Clarke <mikedclarke109@gmail.com>
2014-09-24 00:02:18 +0100

    Project skeleton complete

--- Pyxedit/__init__.py
+++ Pyxedit/__init__.py
@@ -7,15 +7,20 @@ Author: Mike Clarke 13/09/14
 import sys
 from PyQt4 import QtGui
 
+class Window(QtGui.QWidget):
+
+    def __init__(self):
+        super(Window, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('PyxEdit')
+        #self.setWindowIcon(QtGui.QIcon(TODO))
+        self.show()
 def main():
     app = QtGui.QApplication(sys.argv)
-
-    w = QtGui.QWidget()
-    w.resize(800, 600)
-    w.move(300, 300)
-    w.setWindowTitle('Pyxedit')
-    w.show()
-
+    w = Window()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
